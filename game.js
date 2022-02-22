2f02f5a7ac693e5cbe1216acfe05c4a572300349
Ivan Llamas <ip.llamas13@gmail.com>
2022-02-22 01:31:34 -0800

    add scoreboard logic and ability to replay game

--- game.js
+++ game.js
@@ -1,3 +1,31 @@
+//Initalizes and creates scoreboard
+let playerScore = 0;
+let computerScore = 0;
+const playerScorePara = document.querySelector('.player-score');
+const computerScorePara = document.querySelector('.computer-score');
+playerScorePara.textContent = `Player: ${playerScore}`;
+computerScorePara.textContent = `Computer: ${computerScore}`;
+
+//Creates round-by-round results section using DOM manipulation
+const resultsDiv = document.querySelector('.results');
+const playerPara = document.createElement('p');
+const computerPara = document.createElement('p');
+const outcomePara = document.createElement('p');
+playerPara.setAttribute('class', 'player-choice');
+computerPara.setAttribute('class', 'computer-choice');
+outcomePara.setAttribute('class', 'outcome');
+resultsDiv.appendChild(playerPara);
+resultsDiv.appendChild(computerPara);
+resultsDiv.appendChild(outcomePara);
+
+//add event listeners to each button
+const buttons = document.querySelectorAll('.btn');
+console.log(buttons);
+buttons.forEach((btn) => {
+  btn.addEventListener('click', playRound)
+});
+
+
 function computerPlay(){
   //Math.floor() is needed as Math.random() returns a decimal 0 to 1
   //The formula used is Math.random() * (max-min)+min
@@ -16,6 +44,12 @@ function computerPlay(){
 }
 
 function playRound(e) {
+  //If score is already at 5, restart the game
+  if(playerScore === 5 || computerScore === 5){
+    restartGame();
+    return;
+  }
+
   //Take player choice from event target's class
   //and convers first character to upper case
   let playerChoice = e.target.getAttribute('class').slice(4);
@@ -25,8 +59,8 @@ function playRound(e) {
   let computerChoice = computerPlay();
 
   //Declares win/lose messages to display at the end of the round
-  const winMessage = `You Win! ${playerChoice} Beats ${computerChoice}!`
-  const loseMessage = `You Lose! ${computerChoice} Beats ${playerChoice}!`; 
+  const winMessage = `Round Won! ${playerChoice} Beats ${computerChoice}!`
+  const loseMessage = `Round Lost! ${computerChoice} Beats ${playerChoice}!`; 
 
   //Displays the choices on the web page
   playerPara.textContent = `Player chose ${playerChoice}`;
@@ -36,67 +70,83 @@ function playRound(e) {
     outcomePara.textContent = 'Draw!';
     console.log("Draw!");
     return;
-  }
-  
-  if (playerChoice === "Rock") {
+  } else if (playerChoice === "Rock") {
     if (computerChoice === "Scissors") {
       outcomePara.textContent = winMessage;
+      updateScore('player');
       console.log(winMessage);
       return;
     } else {
       outcomePara.textContent = loseMessage;
+      updateScore('computer');
       console.log(loseMessage);
       return;
     }
   } else if (playerChoice === "Scissors") {
     if (computerChoice === "Paper") {
+      updateScore('player');
       outcomePara.textContent = winMessage;
       console.log(winMessage);
       return;
     } else {
       outcomePara.textContent = loseMessage;
+      updateScore('computer');
       console.log(loseMessage);
       return;
     }
   } else if (playerChoice === "Paper") {
     if (computerChoice === "Rock") {
       outcomePara.textContent = winMessage;
+      updateScore('player');
       console.log(winMessage);
       return;
     } else {
       outcomePara.textContent = loseMessage;
+      updateScore('computer');
       console.log(loseMessage);
       return;
     }
   }
 }
 
-//Creates round-by-round results section using DOM manipulation
-const resultsDiv = document.querySelector('.results');
-const playerPara = document.createElement('p');
-const computerPara = document.createElement('p');
-const outcomePara = document.createElement('p');
-playerPara.setAttribute('class', 'player-choice');
-computerPara.setAttribute('class', 'computer-choice');
-outcomePara.setAttribute('class', 'outcome');
-resultsDiv.appendChild(playerPara);
-resultsDiv.appendChild(computerPara);
-resultsDiv.appendChild(outcomePara);
-
-
-
-const buttons = document.querySelectorAll('.btn');
-console.log(buttons);
-buttons.forEach((btn) => {
-  btn.addEventListener('click', playRound)
-});
-
-
-
-
-/* function game(){
-  
-  console.log("Game Over!");
+function updateScore(roundWinner){
+  if (roundWinner === 'player'){
+    playerScore++;
+    playerScorePara.textContent = `Player: ${playerScore}`;
+    if (playerScore === 5) {
+      endGame('player');
+    }
+  } else if (roundWinner === 'computer') {
+    computerScore++;
+    computerScorePara.textContent = `Computer: ${computerScore}`;
+    if (computerScore === 5){
+      endGame('computer');
+    }
+  } else if (roundWinner === 'restart') {
+    playerScorePara.textContent = `Player: ${playerScore}`;
+    computerScorePara.textContent = `Computer: ${computerScore}`;
+  }
 }
 
-game(); */
+function endGame(gameWinner){
+  if (gameWinner === 'player'){
+    outcomePara.textContent = 'Game Over! You Win!';
+  } else {
+    outcomePara.textContent = 'Game Over! You Lose!';
+  }
+  buttons.forEach((btn) => btn.textContent = "Replay");
+  return;
+}
+
+function restartGame(){
+  console.log('Game Restarted');
+  playerScore = 0;
+  computerScore = 0;
+  updateScore('restart');
+  document.querySelector('.rock').textContent = 'Rock';
+  document.querySelector('.paper').textContent = 'Paper';
+  document.querySelector('.scissors').textContent = 'Scissors';
+  playerPara.textContent = '';
+  computerPara.textContent = '';
+  outcomePara.textContent = '';
+}
